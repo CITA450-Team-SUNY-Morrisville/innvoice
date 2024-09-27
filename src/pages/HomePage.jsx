00d3f7beebe49a0b0de138fc8cb42d9c7ba66430
DaVexx <alexanderwoods2019@gmail.com>
2024-09-27 16:02:45 -0400

    Fixed error with hero being lowercase

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Hero from '../components/hero'
+import Hero from '../components/Hero'
 
 const HomePage = () => {
   return (
